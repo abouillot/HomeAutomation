a0f3cdeba76a8f468accf47d0cf8c7b5a64efe98
grumpy-dude <ef.developer1@outlook.com>
2018-09-13 10:56:12 -0700

    ATtiny SPI

---
+++ RFM69HA-Temperature-SensorNode/RFM69NetworkNodeCfg.h
@@ -0,0 +1,4 @@
+#define THIS_RFM69_IS_HIGH_POWER    // Uncomment for RFM69HW/RFM69HCW/etc., comment out if you have RFM69W/RFM69CW/etc.
+
+//**** CHANGE THIS FOR EACH ADDITIONAL SENSOR NODE OR CONTROL NODE ****
+const uint8_t This_Node_ID    = 2;                  // ***THIS MUST BE UNIQUE FOR EACH SENSOR OR CONTROL NODE ON A NETWORK***
---
+++ RFM69HA-Temperature-SensorNode/SensorNode-ATtiny85/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2017-10-21 11:49:12 AM
+ * Author : Me
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
