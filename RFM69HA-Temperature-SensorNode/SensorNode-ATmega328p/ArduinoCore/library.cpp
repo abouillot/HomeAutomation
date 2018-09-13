e00e99855c947d1cb99366a23e90494de1b2c4b1
grumpy-dude <ef.developer1@outlook.com>
2018-09-13 10:53:38 -0700

    ATtiny SPI

---
+++ RFM69HA-Temperature-SensorNode/SensorNode-ATmega328p/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2017-08-07 3:28:57 PM
+ * Author : Me
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
